d92737f64f8071050f538d018357160fd5a7edff
poponta <poponta011218@gmail.com>
2023-10-16 03:16:09 +0900

    Add initialize script for each script

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dir=$(dirname ${0})
+
+alias ecp="${dir}/extended-copy/ecp.sh"
+alias ejobs="${dir}/extended-jobs/ejobs.sh"
+alias pid="${dir}/extended-jobs/ejobs.sh"
+alias objd="${dir}/object-distributor/objdist.sh"
