49900e49c0f1ba154d72a24dfc83248dfadbaaa6
poponta <34686350+poponta1218@users.noreply.github.com>
2023-10-20 01:39:03 +0900

    Delete another alias for ejobs

--- init.sh
+++ init.sh
@@ -4,5 +4,4 @@ dir=$(dirname ${0})
 
 alias ecp="${dir}/extended-copy/ecp.sh"
 alias ejobs="${dir}/extended-jobs/ejobs.sh"
-alias pid="${dir}/extended-jobs/ejobs.sh"
 alias objd="${dir}/object-distributor/objdist.sh"
